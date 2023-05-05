3d08d71914c4afad4efa96df6ac6ea449bb448f5
Poonguzhali001 <132701134+Poonguzhali001@users.noreply.github.com>
2023-05-05 23:46:06 +0530

    Add files via upload
    day1

---
+++ Cars.java
@@ -0,0 +1,79 @@
+package Week1.Day1;
+
+public class Cars {
+
+	public void unlockTheCar() {
+		System.out.println(" unlockTheCar");
+	}
+	
+	public void selfStarts() {
+		System.out.println("use selfStarts");
+	}
+
+	public void putOnSeatBelt() {
+		System.out.println("putOnSeatBelt");
+	}
+	
+	public void adjustMirrors() {
+		System.out.println("adjustMirrors");
+	}
+	
+	public void adjustStiringwheels() {
+		System.out.println("adjustStiringwheels");
+	}
+	
+	public void lockTheDoor() {
+		System.out.println("lockTheDoor");
+	}
+	
+	public void applyGear() {
+		System.out.println("applyGear");
+	}
+
+	public void startDriving() {
+		System.out.println("startDriving");
+	}
+
+	public void switchOnAc() {
+		System.out.println("switchOnAc");
+	}
+
+	public void applyBreak() {
+		System.out.println("applyBreak");
+	}	
+	
+	public void applyAccelerate() {
+		System.out.println("applyAccelerate");
+	}	
+   
+	public void takeReverse() {
+		System.out.println("takeReverse");
+	}	
+	
+	public void useHorn() {
+		System.out.println("useHorn");
+	}
+	
+	
+		public static void main(String[] args) {
+			Cars Cars =new Cars() ;
+	        Cars.unlockTheCar();	
+	        Cars.selfStarts();
+	        Cars.putOnSeatBelt();
+	        Cars.adjustMirrors();
+	        Cars.lockTheDoor();
+	        Cars.applyGear();
+            Cars.startDriving();
+            Cars.switchOnAc();
+            Cars.applyBreak();
+            Cars.applyAccelerate();
+            Cars.takeReverse();
+            Cars.useHorn();
+            
+		}	
+  }  
+            
+            
+            
+            
+	
---
+++ Mobile1.java
@@ -0,0 +1,87 @@
+package Week1.Day1;
+
+public class Mobile1 {
+
+	public void callSomeone() {
+		System.out.println("callSomeone");
+		
+	}
+	public void sendMessage() {
+		System.out.println("sendMessage");
+	}
+	
+	public void useCompass() {
+		System.out.println("useCompass");
+	}
+	
+	public void useTorchlight () {
+		System.out.println("useTorchlight");
+	}
+	
+	public void searchSomething() {
+		System.out.println("searchSomthing");
+	}
+	
+	public void playGames() {
+		System.out.println("playGames");
+	}
+	public void takePhoto() {
+		System.out.println("takePhoto");
+	}
+	
+	public void recordVideo() {
+		System.out.println("recordVideo");
+	}
+	
+	public void useCalculator() {
+		System.out.println("useCalculator");
+	}
+		
+	public void makeNotes() {
+		System.out.println("makeNotes");	
+   }
+	
+	public void clickPhotos() {
+		System.out.println("clickPhotos");
+	}
+	
+	public void makeEnotes() {
+		System.out.println("makeEnotes");
+	}
+		
+	public void useCalenders() {
+		System.out.println("useCalenders");
+	}
+	
+	
+		
+		public static void main(String[] args) {
+		   Mobile1 Mobile1 =new Mobile1() ;
+				Mobile1.callSomeone();
+				Mobile1.sendMessage(); 
+				Mobile1.useCompass();
+				Mobile1.useTorchlight();
+				Mobile1.searchSomething();
+				Mobile1.playGames();
+				Mobile1.takePhoto();   
+				Mobile1.recordVideo();   
+				Mobile1.useCalculator();   
+				Mobile1.makeNotes();   
+				Mobile1.clickPhotos();  
+				Mobile1.makeEnotes();   
+				Mobile1.useCalenders();  
+				   
+				   
+				   
+   }
+
+
+
+
+
+
+
+
+
+
+}
