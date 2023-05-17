7ecc31b920b57f7545b1121c68e6787355e0df6a
Poonguzhali001 <132701134+Poonguzhali001@users.noreply.github.com>
2023-05-17 22:19:44 +0530

    week3.day2

---
+++ Intersection.java
@@ -0,0 +1,19 @@
+package Week3day1;
+
+public class Intersection {
+
+	public static void main(String[] args) {
+		int Array01 []   = {3,2,11,4,6,7};
+        int Array02 []   = {1,2,8,4,9,7};
+	for (int i = 0; i <Array01.length; i++) {
+	for (int j = 0; j <Array02.length; j++) {
+		
+	     if(Array01[i]== Array02[j]) {
+	System.out.println(Array01[i]);
+	
+	}
+	}
+	}
+	}
+
+}
