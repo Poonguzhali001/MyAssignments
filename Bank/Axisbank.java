3f06314cf1e7f86e70eff54169741d0cbc662f9d
Poonguzhali001 <132701134+Poonguzhali001@users.noreply.github.com>
2023-05-30 22:23:48 +0530

    bank.java

---
+++ Bank/Axisbank.java
@@ -0,0 +1,16 @@
+package Bank;
+
+public class Axisbank extends bankinfo{
+
+	public void deposit() {
+	System.out.println("Deposit");
+	}	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Axisbank bank = new Axisbank();
+ 
+  bank.deposit();
+ 
+	}
+	}
+
